7091d7f09595594ace6c8248a2981e4f2c4945e9
GaelTikeng <Gaelinho@yahoo.fr>
2024-02-16 17:28:05 +0100

    added loader when fetching image

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,6 @@ const url = "https://foodish-api.com/images/burger";
 const loaderContainer = document.querySelector(".loader-container");
 loaderContainer.style.display = "none";
 let burgerType = Math.floor(Math.random() * 10);
-// console.log(burgerType)
 const listOfBurger = [
   "burger87.jpg",
   "burger13.jpg",
@@ -18,41 +17,46 @@ const listOfBurger = [
 
 // function to display loader
 const displayLoader = () => {
-  return loaderContainer.style.display = "block";
+  loaderContainer.style.display = "block";
 };
 
 // hide loader
 const hideLoader = () => {
-  return loaderContainer.style.dsiplay = "none";
+  loaderContainer.style.dsiplay = "none";
 };
 
 // function to fetch image
 function fetchImage(mealType) {
-  // displayLoader();
+  loaderContainer.style.display = "block";
   fetch("https://foodish-api.com/images/burger")
     .then((response) => {
+      loaderContainer.style.display = "none";
       console.log(response);
-      hideLoader();
       document.querySelector(".display").innerHTML = `
-      <div>
-        <h1>This is image</h1>
+      <div >
+        <h2>This is image</h2>
         <img src="${response.url}/${mealType}" alt="foodish image"/>
       </div>`;
     })
     .catch((error) => {
       console.log(error);
+      document.querySelector('.display').innerHTML = `
+      <div>
+        <p class="display-error">${error}</p>
+      </div>
+      `
     });
 }
 
 // fetch image when page loads
-fetchImage("burger45.jpg");
+fetchImage("burger61.jpg");
 
 function getData() {
   // displayLoader();
   let burgerType = Math.floor(Math.random() * 10);
   console.log(burgerType);
   console.log("hello gael");
-  fetchImage(listOfBurger[burgerType])
+  fetchImage(listOfBurger[burgerType]);
   // fetch(url)
   //   .then((data) => {
   //     hideLoader();
