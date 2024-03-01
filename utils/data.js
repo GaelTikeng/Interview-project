0a596248de9226ce5b0f69b6df6f50f2d623ce15
GaelTikeng <Gaelinho@yahoo.fr>
2024-03-01 15:27:59 +0100

    some changes on the code

---
+++ utils/data.js
@@ -0,0 +1,12 @@
+export const listOfBurger = [
+  "burger87.jpg",
+  "burger13.jpg",
+  "burger29.jpg",
+  "burger48.jpg",
+  "burger33.jpg",
+  "burger79.jpg",
+  "burger77.jpg",
+  "burger45.jpg",
+  "burger86.jpg",
+  "burger61.jpg",
+];
